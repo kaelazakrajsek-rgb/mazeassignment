caf75c3789e577e61477970b8e8d4061b304e91a
bibingkabunny <kaelazakrajsek@lcad.edu>
2026-09-22 17:55:55 -0700

    Initial commit

---
+++ Assets/Scenes/NewMonoBehaviourScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move += Vector3.right;
+        }
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+}
